dbbf569e2b9c8b9b71fda62284f201dad2c07143
mallexibra <mallexibra@gmail.com>
2023-09-03 22:29:49 +0700

    add: analytics vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AnalyticsProps } from "@vercel/analytics/*";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <AnalyticsProps />
   </React.StrictMode>
 );
